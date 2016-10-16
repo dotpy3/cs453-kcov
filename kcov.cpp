f2e2faa8cec8f255126716d1981b65ec6b661ccc
Eric Gourlaouen <gourlaoueneric@gmail.com>
2016-10-16 15:02:15 +0900

    now working

--- kcov.cpp
+++ kcov.cpp
@@ -11,6 +11,7 @@
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Basic/Diagnostic.h"
 #include "clang/Basic/FileManager.h"
+#include "clang/Basic/LangOptions.h"
 #include "clang/Basic/SourceManager.h"
 #include "clang/Basic/TargetOptions.h"
 #include "clang/Basic/TargetInfo.h"
@@ -42,7 +43,7 @@ private:
     unsigned int lineNumber;
     string condition;
 public:
-    Branch(Stmt* stmt, SourceManager& srcmgr, Expr* cn) : condition(TheRewriter.ConvertToString(cn)) {
+    Branch(Stmt* stmt, SourceManager& srcmgr, string cn) : condition(cn) {
         lineNumber = srcmgr.getExpansionLineNumber(stmt->getLocStart());
     }
 
@@ -57,7 +58,9 @@ private:
     ASTContext *context;
     vector<Branch> branches;
     bool firstFuncInstantation;
-    SourceLocation funcInstantation;
+    SourceLocation funcInstantation, mainFuncInstantiaton;
+    string radical;
+    LangOptions lOpts;
 
     SourceManager& getSrcMngr() {
         return context->getSourceManager();
@@ -69,6 +72,7 @@ public:
         context = &(CI->getASTContext());
         branches = vector<Branch>();
         firstFuncInstantation = false;
+        lOpts = CI->getLangOpts();
 	}
 
 	bool hasImplicitDefault(SwitchStmt *s) {
@@ -84,17 +88,28 @@ public:
         return true;
 	}
 
-    void AddCompleteConditionCoverageEvaluation(unsigned int brNmr, Expr* condition) {
+    void AddCompleteConditionCoverageEvaluation(unsigned int brNmr, Expr* cn) {
         // Adds a coverage evaluation for the brNmr-d branch at the location of the condition
-        string appendBefore = "((";
-        string appendAfter = " || !VisitedElseBranch(" + int_to_string<unsigned int>(brNmr)
-            + ")) && VisitedThenBranch(" + int_to_string<unsigned int>(brNmr) + "))";
-        TheRewriter.InsertTextAfter(condition->getLocStart(), appendBefore);
-        TheRewriter.InsertTextBefore(condition->getLocEnd(), appendAfter);
+        llvm::outs() << "Adding a complete condition coverage evaluation...\n";
+        string newConditionBefore = "BranchValidator(";
+        string newConditionEnd = ", ";
+        newConditionEnd += int_to_string<unsigned int>(brNmr);
+        newConditionEnd += ")";
+        TheRewriter.InsertTextAfter(cn->getLocStart(), newConditionBefore);
+        TheRewriter.InsertTextAfter(Lexer::getLocForEndOfToken(cn->getLocEnd(), 0, getSrcMngr(), lOpts), newConditionEnd);
+    }
+
+    void AddSimpleConditionCoverageEvaluation(unsigned int brNmr, SourceLocation sl) {
+        llvm::outs() << "Adding a simple condition coverage evaluation...\n";
+        string ThenBranchTraversing = "VisitedThenBranch(";
+        ThenBranchTraversing += int_to_string<unsigned int>(brNmr);
+        ThenBranchTraversing += ");\n";
+        TheRewriter.InsertTextAfter(sl, ThenBranchTraversing);
     }
 
     unsigned int addBranch(Branch b) {
         // Adds a branch to the branches list, returns the index of the branch
+        llvm::outs() << "\tAdding branch...\n";
         branches.push_back(b);
         return branches.size() - 1;
     }
@@ -102,48 +117,71 @@ public:
     bool VisitStmt(Stmt *s) {
         // Fill out this function for your homework
         // llvm::outs() << "On visite un statement\n";
+        llvm::outs() << "Visiting statement...\n";
     	if (ForStmt *forS = dyn_cast<ForStmt>(s)) {
     		// For Statement
-            AddCompleteConditionCoverageEvaluation(addBranch(Branch(forS, getSrcMngr(), forS->getCond())), forS->getCond());
+            AddCompleteConditionCoverageEvaluation(addBranch(Branch(forS, getSrcMngr(), TheRewriter.ConvertToString(forS->getCond()))), forS->getCond());
     	} else if (IfStmt *ifS = dyn_cast<IfStmt>(s)) {
     		// For Statement
-            AddCompleteConditionCoverageEvaluation(addBranch(Branch(ifS, getSrcMngr(), ifS->getCond())), ifS->getCond());
+            AddCompleteConditionCoverageEvaluation(addBranch(Branch(ifS, getSrcMngr(), TheRewriter.ConvertToString(ifS->getCond()))), ifS->getCond());
     	} else if (DoStmt *doS = dyn_cast<DoStmt>(s)) {
     		// Do Statement
-            AddCompleteConditionCoverageEvaluation(addBranch(Branch(doS, getSrcMngr(), doS->getCond())), doS->getCond());
+            AddCompleteConditionCoverageEvaluation(addBranch(Branch(doS, getSrcMngr(), TheRewriter.ConvertToString(doS->getCond()))), doS->getCond());
     	} else if (CaseStmt *swC = dyn_cast<CaseStmt>(s)) {
     		// Switch Case Statement
             //addBranch(new Branch(swC, getSrcMngr(), swC->getCond()));
             //nbBranches += 1;
+            // AddSimpleConditionCoverageEvaluation(addBranch(Branch(swC, getSrcMngr(), "case")), swC->getSubStmt()->child_begin()->getLocStart());
+            AddSimpleConditionCoverageEvaluation(addBranch(Branch(swC, getSrcMngr(), TheRewriter.ConvertToString(swC->getLHS()))), swC->getSubStmt()->child_begin()->getLocStart());
     	} else if (ConditionalOperator *ternS = dyn_cast<ConditionalOperator>(s)) {
             // Conditional Operator
-            AddCompleteConditionCoverageEvaluation(addBranch(Branch(ternS, getSrcMngr(), ternS->getCond())), ternS->getCond());
+            AddCompleteConditionCoverageEvaluation(addBranch(Branch(ternS, getSrcMngr(), TheRewriter.ConvertToString(ternS->getCond()))), ternS->getCond());
         } else if (SwitchStmt *swS = dyn_cast<SwitchStmt>(s)) {
     		// Switch Statement
     		if (hasImplicitDefault(swS)) {
-    			//
+                unsigned int nbSwBr = addBranch(Branch(swS, getSrcMngr(), "default"));
+                string addedCCode = "\ndefault: VisitedThenBranch(";
+                addedCCode += int_to_string<unsigned int>(nbSwBr);
+                addedCCode += ");\nbreak;\n";
+    			TheRewriter.InsertTextBefore(swS->getLocEnd(), addedCCode);
     		}
     	} else if (WhileStmt *whS = dyn_cast<WhileStmt>(s)) {
     		// While statement
-            AddCompleteConditionCoverageEvaluation(addBranch(Branch(whS, getSrcMngr(), whS->getCond())), whS->getCond());
+            AddCompleteConditionCoverageEvaluation(addBranch(Branch(whS, getSrcMngr(), TheRewriter.ConvertToString(whS->getCond()))), whS->getCond());
     	} else if (DefaultStmt *defS = dyn_cast<DefaultStmt>(s)) {
     		// Default Statement
+            AddSimpleConditionCoverageEvaluation(addBranch(Branch(defS, getSrcMngr(), "default")), defS->getSubStmt()->child_begin()->getLocStart());
     	}
         return true;
     }
     
     bool VisitFunctionDecl(FunctionDecl *f) {
-        if (!f->isExternC() && firstFuncInstantation == false) {
+        if (f->hasBody() && firstFuncInstantation == false) {
+            llvm::outs() << "First function found - saving location for initialization parts...\n";
             firstFuncInstantation = true;
             funcInstantation = f->getSourceRange().getBegin();
+
         }
         if (f->isMain()) {
+            llvm::outs() << "Main function found - adding branch coverage evaluation result...\n";
             TheRewriter.InsertTextBefore(f->getSourceRange().getEnd(), "\nWriteResult();\n");
+            mainFuncInstantiaton = f->getBody()->child_begin()->getLocStart();
         }
         return true;
     }
 
-    bool WriteInitialization(string FileRadical) {
+    void setRadical(string rad) {
+        radical = rad;
+    }
+
+    bool WriteInitialization() {
+        if (!firstFuncInstantation) {
+            llvm::outs() << "Initialization parts adding failed - cannot find any proper location to do it (no function found in C file).\n";
+            return false;
+        }
+        SourceLocation begin = funcInstantation;
+        //begin = Lexer::getBeginningOfFileToken(getBeginningOfFileToken)
+        llvm::outs() << "Adding initialization parts to C file...\n";
         string StructCreation = "struct BranchTest {\n";
         StructCreation += "\tunsigned int lineNumber;\n";
         StructCreation += "\tunsigned int thenBrExec;\n";
@@ -151,25 +189,31 @@ public:
         StructCreation += "\tchar* condExpression;\n";
         StructCreation += "};\n";
 
-        string BranchTestConstructor = "void InitBranch(BranchTest* br, unsigned int givenLineNumber, char* givenCondExpression) {\n";
-        BranchTestConstructor += "\tbr->lineNumber = givenLineNumber;\n\tbr->condExpression = givenCondExpression;\n\tbr->thenBrExec = 0;\n";
-        BranchTestConstructor += "\tbr->elseBrExec = 0;\n}\n";
+        string BranchValidator = "int BranchValidator(int condition, unsigned int nbBr) {\n\tif (condition) {\n\t\tVisitedThenBranch(nbBr);\n\t} else {\n\t\tVisitedElseBranch(nbBr);\n\t}\n\treturn condition;\n}\n";
 
-        string BranchThenVisitAcceptance = "bool VisitedThenBranch(unsigned int brId) {\n";
+        string BranchTestConstructor = "void InitBranch(struct BranchTest* br, unsigned int nb, unsigned int givenLineNumber, char* givenCondExpression) {\n";
+        BranchTestConstructor += "\tbr[nb].lineNumber = givenLineNumber;\n\tbr[nb].condExpression = givenCondExpression;\n\tbr[nb].thenBrExec = 0;\n";
+        BranchTestConstructor += "\tbr[nb].elseBrExec = 0;\n}\n";
+
+        string BranchThenVisitAcceptance = "int VisitedThenBranch(unsigned int brId) {\n";
         BranchThenVisitAcceptance += "\tall_branches[brId].thenBrExec++;\n";
-        BranchThenVisitAcceptance += "\treturn true;\n";
+        BranchThenVisitAcceptance += "\treturn 1;\n";
         BranchThenVisitAcceptance += "}\n";
 
-        string BranchElseVisitAcceptance = "bool VisitedElseBranch(unsigned int brId) {\n";
+        string BranchElseVisitAcceptance = "int VisitedElseBranch(unsigned int brId) {\n";
         BranchElseVisitAcceptance += "\tall_branches[brId].elseBrExec++;\n";
-        BranchElseVisitAcceptance += "\treturn true;\n";
+        BranchElseVisitAcceptance += "\treturn 1;\n";
         BranchElseVisitAcceptance += "}\n";
 
         string BasicInclude = "#include <stdio.h>\n";
 
-        string NumberOfBranches = "#define NB_BRANCHES " + int_to_string<int>(branches.size()) + "\n\nBranchTest all_branches[NB_BRANCHES];\n";
+        string NumberOfBranches = "\nstruct BranchTest all_branches[";
+        NumberOfBranches += int_to_string<int>(branches.size());
+        NumberOfBranches += "];\n";
 
-        string BranchesConditions = "char* BranchesConditions[NB_BRANCHES] = {";
+        string BranchesConditions = "char *BranchesConditions[";
+        BranchesConditions += int_to_string<int>(branches.size());
+        BranchesConditions += "] = {";
         if (branches.size() > 0) {
             BranchesConditions += "\"";
             BranchesConditions += branches[0].getCondition();
@@ -183,18 +227,34 @@ public:
                 BranchesConditions += branches[i].getCondition();
                 BranchesConditions += "\"";
             }
-            BranchesInit += "InitBranch(&(all_branches["; BranchesInit += int_to_string<unsigned int>(i); BranchesInit += "]), ";
+            BranchesInit += "InitBranch(all_branches, "; BranchesInit += int_to_string<unsigned int>(i); BranchesInit += ", ";
             BranchesInit += int_to_string<unsigned int>(branches[i].getLineNumber()); BranchesInit += ", ";
             BranchesInit += "BranchesConditions["; BranchesInit += int_to_string<unsigned int>(i) + "]);\n";
         }
 
         BranchesConditions += "};\n";
 
-        string WriteResult = "void WriteResult() {\n\tFILE* output = fopen(\"" + FileRadical + "-cov-measure.txt\"";
-        WriteResult += ", \"w+\");\n\tfprintf(output, \"Line\\tThen\\tElse\\tCondition\")\n\tfor (unsigned int j = 0; j < NB_BRANCHES; j++) {\n";
-        WriteResult += "\t\tfprintf(output, (\"%d\\t\", all_branches[j].lineNumber));\n\t\tfprintf(output, (\"%d\\t\", all_branches[j].thenBrExec));\n";
-        WriteResult += "\t\tfprintf(output, (\"%d\\t\", all_branches[j].elseBrExec));\n\t\tfprintf(output, all_branches[j].condExpression);\n";
+        string WriteResult = "void WriteResult() {\n\tFILE* output = fopen(\"" + radical + "-cov-measure.txt\"";
+        WriteResult += ", \"w+\");\n\tfprintf(output, \"Line\\tThen\\tElse\\tCondition\\n\");\n";
+        WriteResult += "\tunsigned int j;\n\tfor (j = 0; j < ";
+        WriteResult += int_to_string<int>(branches.size());
+        WriteResult += "; j++) {\n";
+        WriteResult += "\t\tfprintf(output, \"%d\\t\", all_branches[j].lineNumber);\n\t\tfprintf(output, \"%d\\t\", all_branches[j].thenBrExec);\n";
+        WriteResult += "\t\tfprintf(output, \"%d\\t\", all_branches[j].elseBrExec);\n\t\tfprintf(output, all_branches[j].condExpression);\n";
         WriteResult += "\t\tfprintf(output, \"\\n\");\n\t}\n\tfclose(output);\n}\n";
+
+        TheRewriter.InsertTextBefore(begin, BranchValidator);
+        TheRewriter.InsertTextBefore(begin, WriteResult);
+        TheRewriter.InsertTextAfter(mainFuncInstantiaton, BranchesInit);
+        TheRewriter.InsertTextBefore(begin, BranchesConditions);
+        TheRewriter.InsertTextBefore(begin, BranchElseVisitAcceptance);
+        TheRewriter.InsertTextBefore(begin, BranchThenVisitAcceptance);
+        TheRewriter.InsertTextBefore(begin, BranchTestConstructor);
+        TheRewriter.InsertTextBefore(begin, NumberOfBranches);
+        TheRewriter.InsertTextBefore(begin, StructCreation);
+        //TheRewriter.InsertTextBefore(begin, BasicInclude);
+
+        return true;
     }
 };
 
@@ -213,8 +273,12 @@ public:
         return true;
     }
 
-    bool WriteInitialization(string FileRadical) {
-        return Visitor.WriteInitialization(FileRadical);
+    void setRadical(string FileRadical) {
+        Visitor.setRadical(FileRadical);
+    }
+
+    void writeInit() {
+        Visitor.WriteInitialization();
     }
 
 private:
@@ -298,7 +362,7 @@ int main(int argc, char *argv[])
     TheRewriter.setSourceMgr(SourceMgr, TheCompInst.getLangOpts());
 
     string origFileName = argv[1];
-    string radical = origFileName.substr(0, origFileName.find_last_of(".c"));
+    string radical = origFileName.substr(0, origFileName.find_last_of(".c") - 1);
     string newCFileName = radical + "-cov.c";
 
     // Set the main file handled by the source manager to the input file.
@@ -310,17 +374,21 @@ int main(int argc, char *argv[])
     
     // Create an AST consumer instance which is going to get called by ParseAST.
     MyASTConsumer TheConsumer(&TheCompInst);
+    TheConsumer.setRadical(radical);
+
+    llvm::outs() << "Beginning parsing...\n";
 
     // Parse the file to AST, registering our consumer as the AST consumer.
     ParseAST(TheCompInst.getPreprocessor(), &TheConsumer, TheCompInst.getASTContext());
 
-    // Add the first process
-    TheConsumer.WriteInitialization(radical);
+    TheConsumer.writeInit();
 
     const RewriteBuffer *RewriteBuf = TheRewriter.getRewriteBufferFor(SourceMgr.getMainFileID());
     ofstream output(newCFileName.c_str());
     output << string(RewriteBuf->begin(), RewriteBuf->end());
     output.close();
+
+    llvm::outs() << "Done !\n";
 
     return 0;
 }
